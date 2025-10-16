9165688dc14107e83808b3aacc596685b8e40ae2
James Corey <james@ironview360.com>
2025-10-16 11:05:13 -0400

    added home and privacy pages and redirects for netlify

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const HomePage = () => {
+	return (
+		<>
+			<h1>Woodworker's Companion Web</h1>
+			<Link to="/privacy-policy">Privacy Policy</Link>
+		</>
+	)
+}
+
+export default HomePage
---
+++ src/components/PrivacyPolicy.tsx
@@ -0,0 +1,9 @@
+const PrivacyPolicy = () => {
+  return (
+    <>
+    <h2>Privacy Policy</h2>
+    </>
+  )
+}
+
+export default PrivacyPolicy
