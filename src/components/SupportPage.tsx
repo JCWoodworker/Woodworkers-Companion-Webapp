a907abd61060c8c924bafb1a5b86de821fa01277
James Corey <james@ironview360.com>
2025-10-16 11:26:41 -0400

    added support page

---
+++ src/components/SupportPage.tsx
@@ -0,0 +1,14 @@
+const SupportPage = () => {
+	return (
+		<div className="page-container">
+			<h1>Support</h1>
+      <p>If you have any questions or feedback, please contact us at:</p>
+      <a href="mailto:woodworkerscompanion@gmail.com">
+        woodworkerscompanion@gmail.com
+      </a>
+      <p>We will do our best to respond to your inquiry as soon as possible.</p>
+		</div>
+	)
+}
+
+export default SupportPage
