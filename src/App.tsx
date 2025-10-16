769250deeb72fbf44fbb1a526d4137516ad09f64
James Corey <james@ironview360.com>
2025-10-16 11:27:17 -0400

    added route for support page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css"
 import HomePage from "./components/HomePage"
 import PrivacyPolicyPage from "./components/PrivacyPolicy"
 import { Route, Routes } from "react-router-dom"
+import SupportPage from "./components/SupportPage"
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+				<Route path="/support" element={<SupportPage />} />
 			</Routes>
 		</>
 	)
